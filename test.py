fbd37fc40e8ac86c04cbb634d0c10585a7aa51c9
evanmaa <evanmayx7@gmail.com>
2026-09-19 23:53:25 -0400

    testing motor more

--- test.py
+++ test.py
@@ -1,49 +1,57 @@
 import subprocess
-import time
 
 PINS = [17, 18, 27, 22]
 
 SEQUENCE = [
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1],
     [1, 0, 0, 1],
+    [1, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 1],
 ]
 
-def set_pin(pin, value):
-    subprocess.run([
-        "gpio-rp1",
-        "set",
-        str(pin),
-        "dh" if value else "dl"
-    ], check=True)
+current_state = [0, 0, 0, 0]
 
 
-def set_motor(state):
-    for pin, value in zip(PINS, state):
-        set_pin(pin, value)
+def gpio(pin, value):
+    subprocess.run(
+        [
+            "gpio-rp1",
+            "set",
+            str(pin),
+            "dh" if value else "dl",
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
 
-# Configure GPIO
+def set_motor(new_state):
+    global current_state
+
+    for i in range(4):
+
+        # Only touch GPIO if it actually changed
+        if new_state[i] != current_state[i]:
+            gpio(PINS[i], new_state[i])
+
+    current_state = new_state.copy()
+
+
+# Configure outputs once
 for pin in PINS:
-    subprocess.run([
-        "gpio-rp1", "set", str(pin), "op", "pn", "dl"
-    ], check=True)
+    subprocess.run(
+        ["gpio-rp1", "set", str(pin), "op", "pn", "dl"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
 
-print("Motor moving...")
+# Run motor
+for _ in range(500):
 
-# Move a small amount
-for _ in range(20):
     for state in SEQUENCE:
         set_motor(state)
-        time.sleep(0.01)
 
-# Release motor
+
+# Shut motor off
 set_motor([0, 0, 0, 0])
-
-print("Done")
